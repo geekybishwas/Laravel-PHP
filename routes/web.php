14c2fb75ad766c4453391a11400159852fe52052
geekybishwas <techiebishwas@gmail.com>
2024-05-04 14:50:35 +0545

    add model

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
+});
+
+Route::get('/hello',function(){
+
+    return "Hello page ma aako ho?";
 });
