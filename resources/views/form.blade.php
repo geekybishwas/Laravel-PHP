b2ae7e55a4486541a6f0c6cdf445a3e5181d0132
geekybishwas <techiebishwas@gmail.com>
2024-05-08 23:34:34 +0545

    use of component for reuisbale

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,24 +6,13 @@
     {{-- url('/''),it gets the base url so the action of this form goes to baseurl/register with method of post --}}
     <form action="{{url('/')}}/register" method="POST"> 
         @csrf
-        <label for="name">Name</label>
-        <input type="text" name='name' id='name'>
-
-        <label for="email">Email</label>
-        <input type="text" name='email' id='email'>
-
-        <label for="password">Password</label>
-        <input type="password" name='password' id='password'>
-        @error('password')
-            {{$message}}
-        @enderror
-
-        <label for="cpassword">Password</label>
-        <input type="password" name='password_confirmation' id='password_confirmation'>
-        @error('password_confirmation')
-            {{$message}}
-        @enderror
-
+        @php    
+            $demo=1;
+        @endphp
+        <x-input label='Name' name="name" type='text':demo='$demo'/>
+        <x-input label='Email' name="email" type='email'/>
+        <x-input label='Password' name="password" type='password'/>
+        <x-input label='Confirm Password' name="confirmation_password" type='password'/>
         <button>Submit</button>
     </form>
 @endsection
